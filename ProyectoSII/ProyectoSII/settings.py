766900dd2f5a851956efa4cc205c05f8427daab3
GabrielFig <gabriggel2000@gmail.com>
2021-06-02 15:43:49 -0600

    edit settings

--- ProyectoSII/ProyectoSII/settings.py
+++ ProyectoSII/ProyectoSII/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ProyectoSII.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'escuelaDb',
+        'USER': 'gabriel',
+        'PASSWORD': '180300ga',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
